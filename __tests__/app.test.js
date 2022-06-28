78d3e38b240a9c82e6845d3b503adb70fa3cb830
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-28 16:53:09 -0700

    second test passing for posting to sessions--logging user in

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,19 +2,36 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 
 const mockUser = {
   email: 'test@example.com',
   password: '1234567890',
 };
+//what exactly is happening here with userProps?
+const handleSignIn = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  
+  return [agent, user];
+  
+};
 
-describe('backend-express-template routes', () => {
+describe('users', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  
+  afterAll(() => {
+    pool.end();
+  });
 
-  it('should create a new user', async () => {
+  it('creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     const { email } = mockUser;
 
@@ -24,8 +41,14 @@ describe('backend-express-template routes', () => {
     });
   });
 
+  it('logs in a user', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app).post('/api/v1/users/sessions').send(mockUser);
+    expect(res.body.message).toEqual('Signed in successfully!');
 
-  afterAll(() => {
-    pool.end();
   });
 });
+    
+    
+
+  
