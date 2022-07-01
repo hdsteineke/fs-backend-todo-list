99f7803f9c0dfdd8a42a12872b2770faf64543e8
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-07-01 11:23:16 -0700

    passing test for Todo.updateById()

--- __tests__/todo.test.js
+++ __tests__/todo.test.js
@@ -28,7 +28,7 @@ describe('users', () => {
     expect(res.body.task).toEqual('Gather eggs');
   });
 
-  ('should update a particular todo', async () => {
+  it('should update a particular todo', async () => {
     const res = await request(app).put('/api/v1/todos/2').send({ task: 'Remember to feed chickens' });
     expect(res.status).toEqual(200);
     expect(res.body.id).toEqual('2');
